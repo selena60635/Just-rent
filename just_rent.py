ab3d6742f635701f5375b86266fdbfdf49c70032
selena60635 <selena606352000@gmail.com>
2024-04-24 00:03:14 +0800

    add DB migrate

--- just_rent.py
+++ just_rent.py
@@ -1,6 +1,6 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
-# from app.models import booking, user, cars, location
+from app.models import user, car, location, booking
 
 app = create_app()
