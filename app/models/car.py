4b5ee3465c054ed9a2a2eeaea1d88beda298ba4e
selena60635 <selena606352000@gmail.com>
2024-04-26 21:39:36 +0800

    modify col name of the car table model

--- app/models/car.py
+++ app/models/car.py
@@ -6,9 +6,7 @@ from datetime import datetime, timezone
 class Car(db.Model):
     __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
-
     name = db.Column(db.String(255), nullable=False, default='Unknown')
-
     displacement = db.Column(db.Integer, nullable=False, default=0)
     body = db.Column(db.String(255), nullable=False, default='Unknown')
     seat = db.Column(db.Integer, nullable=False, default=0)
@@ -25,5 +23,4 @@ class Car(db.Model):
     last_return_time = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
     like_users = db.Column(db.Integer, db.ForeignKey('users.id'))
-
     
